51518426e5c8075835a7e4c2995d7f1e65ab7109
Juan Miguel <juanmiguelriveroyhornos@gmail.com>
2026-02-04 14:09:54 -0300

    Wednesday

--- app/page.js
+++ app/page.js
@@ -187,7 +187,6 @@ export default function Home() {
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
-          className="hidden md:block"
         >
           <CrazyVideo />
         </motion.div>
